fc11772f2759d111dddeb6a11cd6d0277bd17e3b
BlackRussianCR <40475874+BlackRussianCR@users.noreply.github.com>
2019-03-30 12:09:31 -0600

    Add files via upload

---
+++ Semana 1/Practica_4.py
@@ -0,0 +1,9 @@
+# Verificar el tipo de dato de una variable
+
+Variable_1=1234
+Variable_2="1234"
+Variable_3="Prueba"
+
+print("El tipo de dato para: ", Variable_1,  type(Variable_1))
+print("El tipo de dato para: ", Variable_2,  type(Variable_2))
+print("El tipo de dato para: ", Variable_3,  type(Variable_3))
