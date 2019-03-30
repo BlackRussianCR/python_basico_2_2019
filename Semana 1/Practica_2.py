cb18057522da8f7530275edefcc8cc18db65bd78
BlackRussianCR <40475874+BlackRussianCR@users.noreply.github.com>
2019-03-30 11:07:23 -0600

    Add files via upload

---
+++ Semana 1/Practica_2.py
@@ -0,0 +1,16 @@
+# Esta es la practica para escribir 2 lineas
+# Forma 1
+print("Hola mundo")
+print("Buenos dias")
+
+# Forma 2
+print("Primera linea", "Segunda linea")
+
+# Forma 3
+print("Tercera linea"); print("Cuarta linea")
+
+# Forma 4
+print("Quinta linea\n","Sexta linea")
+
+# Forma 5
+print("Septima Linea\nOctava Linea")
