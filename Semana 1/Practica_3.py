93d13f96c9bbe527983a7af3a3ca2c89447c446a
BlackRussianCR <40475874+BlackRussianCR@users.noreply.github.com>
2019-03-30 12:04:05 -0600

    Add files via upload

---
+++ Semana 1/Practica_3.py
@@ -0,0 +1,15 @@
+# Para verifica el tipo de dato de la variable
+
+Variable_1=5555
+print(Variable_1)
+
+# Caso 1
+print("El valor de la variable llamada 'Variable_1' es", Variable_1)
+
+# Caso 2
+print('El valor de la variable llamada "Variable_1" es', Variable_1)
+
+# Caso 3
+print("El valor de la variable llamada \"Variable_1\" es", Variable_1)
+
+print(type(Variable_1))
