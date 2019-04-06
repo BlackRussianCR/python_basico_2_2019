e9bca4c9bc80e27c6a857e87e58f2f840648644b
BlackRussianCR <40475874+BlackRussianCR@users.noreply.github.com>
2019-04-06 12:04:36 -0600

    Add files via upload

---
+++ Semana_2/Semana_2_Practica_3.py
@@ -0,0 +1,29 @@
+#Uso de split
+## "la casa es mia".split(' ')
+## "la_casa_es_mia".split('_')
+
+#Ejercicio 1
+anno = str(2019)
+hola = "Hola "
+frase = hola + anno
+print(frase)
+
+#Ejercicio 2
+num1 = "3"
+num2 = "0.14"
+num_dec1 = float(num1)
+num_dec2 = float(num2)
+print(num_dec1 + num_dec2)
+
+#Ejercicio 3
+frase = "Hola_a_todos"
+lista = frase.split('_')
+print(lista)
+
+#Ejercicio 4
+mensaje = "Bienvenidos"
+mensaje2 = set(mensaje)
+print(mensaje2)
+
+
+
